304f00aaae5405cdf5fd6a3894ef88aa83cffc7e
prolinked24-source <prolinked24@gmail.com>
2025-11-22 16:24:30 +0100

    Update CandidateLanguage.php

--- backend/app/Models/CandidateLanguage.php
+++ backend/app/Models/CandidateLanguage.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CandidateLanguage extends Model
 {
-    //
+    protected $fillable = [
+        'candidate_profile_id',
+        'language',
+        'level',
+    ];
+
+    public function profile()
+    {
+        return $this->belongsTo(CandidateProfile::class, 'candidate_profile_id');
+    }
 }
