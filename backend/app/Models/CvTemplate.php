08c09e923a36b40618364314f158a73ef17edd30
prolinked24-source <prolinked24@gmail.com>
2025-11-22 16:23:22 +0100

    Update CvTemplate.php

--- backend/app/Models/CvTemplate.php
+++ backend/app/Models/CvTemplate.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class CvTemplate extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'industry',
+        'language',
+        'layout_type',
+        'description',
+    ];
 }
