32262e04d0d2a85c07a412653f88145a3be24a74
prolinked24-source <prolinked24@gmail.com>
2025-11-22 16:22:18 +0100

    Update 2025_11_22_142333_create_candidate_skills_table.php

--- backend/database/migrations/2025_11_22_142333_create_candidate_skills_table.php
+++ backend/database/migrations/2025_11_22_142333_create_candidate_skills_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('candidate_skills', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('candidate_profile_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('level')->nullable(); // z.B. "basic", "good", "expert"
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('candidate_skills');
