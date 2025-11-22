e64a5dd911d8d8ce750cce5f810ab6b10ac610a1
prolinked24-source <prolinked24@gmail.com>
2025-11-22 16:21:47 +0100

    Update 2025_11_22_142332_create_candidate_experiences_table.php

--- backend/database/migrations/2025_11_22_142332_create_candidate_experiences_table.php
+++ backend/database/migrations/2025_11_22_142332_create_candidate_experiences_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('candidate_experiences', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('candidate_profile_id')->constrained()->onDelete('cascade');
+            $table->string('company_name');
+            $table->string('position');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->boolean('is_current')->default(false);
+            $table->text('description')->nullable(); // Bulletpoints
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('candidate_experiences');
