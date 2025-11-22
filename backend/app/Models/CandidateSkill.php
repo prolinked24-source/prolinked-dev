4e30d65ce8368ccdeba40ddbc68e88547290c31e
prolinked24-source <prolinked24@gmail.com>
2025-11-22 16:24:12 +0100

    Update CandidateSkill.php

--- backend/app/Models/CandidateSkill.php
+++ backend/app/Models/CandidateSkill.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CandidateSkill extends Model
 {
-    //
+    protected $fillable = [
+        'candidate_profile_id',
+        'name',
+        'level',
+    ];
+
+    public function profile()
+    {
+        return $this->belongsTo(CandidateProfile::class, 'candidate_profile_id');
+    }
 }
