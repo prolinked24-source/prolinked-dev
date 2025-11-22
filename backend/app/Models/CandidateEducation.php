311968015a8ec5ab0ac17367422e6f68aabad900
prolinked24-source <prolinked24@gmail.com>
2025-11-22 16:23:58 +0100

    Update CandidateEducation.php

--- backend/app/Models/CandidateEducation.php
+++ backend/app/Models/CandidateEducation.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class CandidateEducation extends Model
 {
-    //
+    protected $fillable = [
+        'candidate_profile_id',
+        'institution',
+        'degree',
+        'field_of_study',
+        'start_date',
+        'end_date',
+    ];
+
+    public function profile()
+    {
+        return $this->belongsTo(CandidateProfile::class, 'candidate_profile_id');
+    }
 }
