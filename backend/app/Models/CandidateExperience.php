fdda9647543b95abd1b421dc8f1e887eef1c9f38
prolinked24-source <prolinked24@gmail.com>
2025-11-22 16:23:42 +0100

    Update CandidateExperience.php

--- backend/app/Models/CandidateExperience.php
+++ backend/app/Models/CandidateExperience.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class CandidateExperience extends Model
 {
-    //
+    protected $fillable = [
+        'candidate_profile_id',
+        'company_name',
+        'position',
+        'start_date',
+        'end_date',
+        'is_current',
+        'description',
+    ];
+
+    public function profile()
+    {
+        return $this->belongsTo(CandidateProfile::class, 'candidate_profile_id');
+    }
 }
